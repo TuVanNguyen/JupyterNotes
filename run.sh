4f8fe40b2ccc1f9e657affff70e4c50e48adadff
TuVanNguyen <tuvannguyen33@gmail.com>
2020-06-12 14:30:01 -0500

    backing up jupyter notebooks

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 BASEDIR=$(dirname $0) #go to directory where script is located
 cd $BASEDIR
 sudo git pull
-(sleep 4; xdg-open $(cat log.out | grep http | tail -n 1 | awk '{print $NF}')) & #redirect to jupyter webapp
+(sleep 4; xdg-open $(cat logs/log.out | grep http | tail -n 1 | awk '{print $NF}')) & #redirect to jupyter webapp
 sudo docker-compose up --remove-orphans | tee logs/log.out #redirect docker output to log.out
 sudo docker-compose down
 sudo git add .
